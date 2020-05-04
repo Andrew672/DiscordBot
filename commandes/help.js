5b569d2fbd40ebf7c68b5a7d891e46b9fded7348
Andrew672 <amarbach5@gmail.com>
2020-05-04 15:24:10 +0200

    Ajout infos dans !help

---
+++ commandes/help.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message) =>{
+    message.channel.send({embed:{
+        color:'#ba9704',
+        
+        title : 'Voici les différentes commandes :',
+        url :"https://www.instagram.com/a_mbch/",
+        description:"Pour l'instant, il'y a très peu de commandes, n'hésitez pas à me donner des idées",
+        fields:[{
+            name : '!create cours <matière> <professeur> <élèves concernés(@role)> <heure> <date> <durée> <lien du cours>',
+            value : "Cette commande permet d'indiquer aux élèves concernés, la programmation d'un cours."+ "\nPour utiliser cette commande, je vous propose un exemple" + 
+            " \n!create cours @MonsieurSurrin @élèves 15h30 02/05 2 https://google.fr"+
+            "\nDonc on annonce ici , un cours de Français à touts les élèves le 02/05 à 15h30 pour une durée de 2h"
+            },{
+            name : '!liens',
+            value : "Cette commande permet d'afficher tous les liens des cours des différentes matières"
+            }],
+        timestamp : new Date(),
+        footer:{
+            icon_url:"https://i.imgur.com/1jpGDRw.jpg",
+            url:"https://www.instagram.com/a_mbch/",
+            text:"Crée par Andrew M"
+        }}})
+}
+
+module.exports.help = {
+    name: "help"
+}
